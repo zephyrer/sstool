d28387c36c08e63bdeb4bf39675a49d251ecfb66
janliz.gs@gmail.com <janliz.gs@gmail.com@2ad15014-666a-1afd-9dac-66819fcf0e88>
2013-06-30 08:01:15 +0000

    [what] 提交原始代码

---
+++ trunk/SSTool/SSToolDlg.h
@@ -0,0 +1,52 @@
+
+// SSToolDlg.h : header file
+//
+
+#pragma once
+#include "afxwin.h"
+#include"resource.h"
+typedef enum
+{
+	COM1=0,
+	COM2,
+	COM3,
+	COM4,
+	COM5,
+	COM6,
+	COM7,
+	COM8,
+	COM9,
+}COMM_LIST;
+
+// CSSToolDlg dialog
+class CSSToolDlg : public CDialogEx
+{
+// Construction
+public:
+	CSSToolDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_SSTOOL_DIALOG};
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+// Implementation
+protected:
+	HICON m_hIcon;
+	int	  m_iCurConn;
+	CString m_RecieveData;
+	void InitCommList();
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CComboBox m_ctrComList;
+	afx_msg void OnBnClickedButtonCon();
+	void  OutMsg(CString strMsg);
+	afx_msg void OnBnClickedButtonDisconn();
+	CEdit m_ctlMsgOut;
+	afx_msg void OnBnClickedButtonClear();
+};
