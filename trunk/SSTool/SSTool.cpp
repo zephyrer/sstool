d3583061cc10d80e26b93c6d1c6705a1f311d3e0
janliz.gs@gmail.com <janliz.gs@gmail.com@2ad15014-666a-1afd-9dac-66819fcf0e88>
2013-12-16 11:48:56 +0000

    [what] 1.去掉剪切、全选按钮。改复制为一键全选复制 [how] [why]

--- trunk/SSTool/SSTool.cpp
+++ trunk/SSTool/SSTool.cpp
@@ -67,7 +67,7 @@ BOOL CSSToolApp::InitInstance()
 	// such as the name of your company or organization
 	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
 
-	CSSToolDlg dlg(L"SSTool-zjl@hikeen.com.cn");
+	CSSToolDlg dlg(L"SSTool:zjl@hikeen.com.cn");
 	m_pMainWnd = &dlg;
 	INT_PTR nResponse = dlg.DoModal();
 	if (nResponse == IDOK)
