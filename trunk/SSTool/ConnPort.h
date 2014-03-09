2ddd7b285b4964bfe4e6e2b465e40e95b264bae8
janliz.gs@gmail.com <janliz.gs@gmail.com@2ad15014-666a-1afd-9dac-66819fcf0e88>
2014-03-09 15:37:39 +0000

    [what] 1.增加读取配置文件的功能 [how] [why]

--- trunk/SSTool/ConnPort.h
+++ trunk/SSTool/ConnPort.h
@@ -1,6 +1,46 @@
 #pragma once
 typedef unsigned int Uint;
 
+typedef enum
+{
+	COM1=0,
+	COM2,
+	COM3,
+	COM4,
+	COM5,
+	COM6,
+	COM7,
+	COM8,
+	COM9,
+}COMM_LIST;
+
+typedef enum
+{
+	BAUD110=0,
+	BAUD300,
+	BAUD600,
+	BAUD1200,
+	BAUD2400,
+	BAUD4800,
+	BAUD9600,
+	BAUD14400,
+	BAUD19200,
+	BAUD38400,
+	BAUD57600,
+	BAUD115200,
+	BAUD128000,
+	BAUD256000,
+
+}COM_Baudrate_t;
+
+typedef enum
+{
+	DATABIT6=0,
+	DATABIT7=1,
+	DATABIT8=2,
+}COM_DataBits_t;
+
+
 class ConnPort
 {
 public:
